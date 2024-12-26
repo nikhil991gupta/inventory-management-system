f2539223099d01220cf37f2bf1570d49bd7ccf32
G, Shubham Gupta [Non-Kenvue] <SG03@kenvue.com>
2024-12-26 17:10:44 +0530

    first commit

---
+++ src/components/bodyComponents/Home/SalesByCity.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { Box } from "@mui/material";
+import ApexCharts from "react-apexcharts";
+
+export default function SalesByCity() {
+  const donutOption = {
+    labels: ["Oujda", "Nador", "Berkan", "Casablanca"],
+    legend: {
+      position: "right",
+      fontSize: "14",
+
+      customLegendItems: [
+        "Ghazipur <b>30.3%</b>",
+        "Muhammadabad <b>37.9%</b>",
+        "Ballia <b>9.0%</b>",
+        "Varanasi <b>22.8%</b>",
+      ],
+      //   const total = data.reduce((sum, value) => sum + value, 0);
+      // const percentages = data.map(value => ((value / total) * 100).toFixed(2) + '%');
+    },
+    title: {
+      text: "Sales By City",
+    },
+  };
+  const donutSeries = [44, 55, 13, 33];
+
+  return (
+    <Box
+      sx={{
+        margin: 3,
+        bgcolor: "white",
+        borderRadius: 2,
+        padding: 3,
+        height: "100%",
+      }}
+    >
+      <ApexCharts
+        options={donutOption}
+        series={donutSeries}
+        type="pie"
+        width="100%"
+      />
+    </Box>
+  );
+}
---
+++ src/components/bodyComponents/customer/Customers.js
@@ -0,0 +1,190 @@
+import productList from "../inventory/productList";
+
+const customers = [{
+    id:1,firstName: "Shubham",
+    lastName: "Gupta",
+    position: "Software Engineer",
+    mobile: "9876543210",
+    orders:[{
+        id: 1,
+        products: [{quantity:5,product:productList[0]}, {quantity:5,product:productList[1]},{quantity:5,product: productList[2]}],}
+    ]
+},{
+    id:2,firstName: "Shubham",
+    lastName: "Gupta",
+    position: "Software Engineer",
+    mobile: "9876543210",
+    orders:[{
+        id: 1,
+        products: [{quantity:5,product:productList[0]}, {quantity:5,product:productList[1]},{quantity:5,product: productList[2]}],}
+    ]
+}
+,{
+    id:3,firstName: "Shubham",
+    lastName: "Gupta",
+    position: "Software Engineer",
+    mobile: "9876543210",
+    orders:[{
+        id: 1,
+        products: [{quantity:5,product:productList[0]}, {quantity:5,product:productList[1]},{quantity:5,product: productList[2]}],}
+    ]
+},{
+    id:4,firstName: "Shubham",
+    lastName: "Gupta",
+    position: "Software Engineer",
+    mobile: "9876543210",
+    orders:[{
+        id: 1,
+        products: [{quantity:5,product:productList[0]}, {quantity:5,product:productList[1]},{quantity:5,product: productList[2]}],}
+    ]
+},{
+    id:5,firstName: "Shubham",
+    lastName: "Gupta",
+    position: "Software Engineer",
+    mobile: "9876543210",
+    orders:[{
+        id: 1,
+        products: [{quantity:5,product:productList[0]}, {quantity:5,product:productList[1]},{quantity:5,product: productList[2]}],}
+    ]
+}
+,{
+    id:6,firstName: "Shubham",
+    lastName: "Gupta",
+    position: "Software Engineer",
+    mobile: "9876543210",
+    orders:[{
+        id: 1,
+        products: [{quantity:5,product:productList[0]}, {quantity:5,product:productList[1]},{quantity:5,product: productList[2]}],}
+    ]
+},{
+    id:7,firstName: "Shubham",
+    lastName: "Gupta",
+    position: "Software Engineer",
+    mobile: "9876543210",
+    orders:[{
+        id: 1,
+        products: [{quantity:5,product:productList[0]}, {quantity:5,product:productList[1]},{quantity:5,product: productList[2]}],}
+    ]
+},{
+    id:8,firstName: "Shubham",
+    lastName: "Gupta",
+    position: "Software Engineer",
+    mobile: "9876543210",
+    orders:[{
+        id: 1,
+        products: [{quantity:5,product:productList[0]}, {quantity:5,product:productList[1]},{quantity:5,product: productList[2]}],}
+    ]
+}
+,{
+    id:9,firstName: "Shubham",
+    lastName: "Gupta",
+    position: "Software Engineer",
+    mobile: "9876543210",
+    orders:[{
+        id: 1,
+        products: [{quantity:5,product:productList[0]}, {quantity:5,product:productList[1]},{quantity:5,product: productList[2]}],}
+    ]
+},{
+    id:10,firstName: "Shubham",
+    lastName: "Gupta",
+    position: "Software Engineer",
+    mobile: "9876543210",
+    orders:[{
+        id: 1,
+        products: [{quantity:5,product:productList[0]}, {quantity:5,product:productList[1]},{quantity:5,product: productList[2]}],}
+    ]
+},{
+    id:11,firstName: "Shubham",
+    lastName: "Gupta",
+    position: "Software Engineer",
+    mobile: "9876543210",
+    orders:[{
+        id: 1,
+        products: [{quantity:5,product:productList[0]}, {quantity:5,product:productList[1]},{quantity:5,product: productList[2]}],}
+    ]
+},{
+    id:12,firstName: "Shubham",
+    lastName: "Gupta",
+    position: "Software Engineer",
+    mobile: "9876543210",
+    orders:[{
+        id: 1,
+        products: [{quantity:5,product:productList[0]}, {quantity:5,product:productList[1]},{quantity:5,product: productList[2]}],}
+    ]
+}
+,{
+    id:13,firstName: "Shubham",
+    lastName: "Gupta",
+    position: "Software Engineer",
+    mobile: "9876543210",
+    orders:[{
+        id: 1,
+        products: [{quantity:5,product:productList[0]}, {quantity:5,product:productList[1]},{quantity:5,product: productList[2]}],}
+    ]
+},{
+    id:14,firstName: "Shubham",
+    lastName: "Gupta",
+    position: "Software Engineer",
+    mobile: "9876543210",
+    orders:[{
+        id: 1,
+        products: [{quantity:5,product:productList[0]}, {quantity:5,product:productList[1]},{quantity:5,product: productList[2]}],}
+    ]
+},{
+    id:15,firstName: "Shubham",
+    lastName: "Gupta",
+    position: "Software Engineer",
+    mobile: "9876543210",
+    orders:[{
+        id: 1,
+        products: [{quantity:5,product:productList[0]}, {quantity:5,product:productList[1]},{quantity:5,product: productList[2]}],}
+    ]
+}
+,{
+    id:16,firstName: "Shubham",
+    lastName: "Gupta",
+    position: "Software Engineer",
+    mobile: "9876543210",
+    orders:[{
+        id: 1,
+        products: [{quantity:5,product:productList[0]}, {quantity:5,product:productList[1]},{quantity:5,product: productList[2]}],}
+    ]
+},{
+    id:17,firstName: "Shubham",
+    lastName: "Gupta",
+    position: "Software Engineer",
+    mobile: "9876543210",
+    orders:[{
+        id: 1,
+        products: [{quantity:5,product:productList[0]}, {quantity:5,product:productList[1]},{quantity:5,product: productList[2]}],}
+    ]
+},{
+    id:18,firstName: "Shubham",
+    lastName: "Gupta",
+    position: "Software Engineer",
+    mobile: "9876543210",
+    orders:[{
+        id: 1,
+        products: [{quantity:5,product:productList[0]}, {quantity:5,product:productList[1]},{quantity:5,product: productList[2]}],}
+    ]
+}
+,{
+    id:19,firstName: "Shubham",
+    lastName: "Gupta",
+    position: "Software Engineer",
+    mobile: "9876543210",
+    orders:[{
+        id: 1,
+        products: [{quantity:5,product:productList[0]}, {quantity:5,product:productList[1]},{quantity:5,product: productList[2]}],}
+    ]
+},{
+    id:20,firstName: "Shubham",
+    lastName: "Gupta",
+    position: "Software Engineer",
+    mobile: "9876543210",
+    orders:[{
+        id: 1,
+        products: [{quantity:5,product:productList[0]}, {quantity:5,product:productList[1]},{quantity:5,product: productList[2]}],}
+    ]
+}];
+export default customers;
